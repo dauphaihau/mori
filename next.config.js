4d7f20ba7604dc93f910ab328d7bcf3b9b4f8a57
hau <hau.tvc@intelin.vn>
2022-11-16 20:43:10 +0700

    enable CORS

--- next.config.js
+++ next.config.js
@@ -11,6 +11,20 @@ const nextConfig = {
     NEXT_PUBLIC_MONGODB_URI: process.env.NEXT_PUBLIC_MONGODB_URI,
     NEXT_PUBLIC_JWT_SECRET: process.env.NEXT_PUBLIC_JWT_SECRET
   },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
     // Perform customizations to webpack config
@@ -30,7 +44,6 @@ const nextConfig = {
     // !! WARN !!
     ignoreBuildErrors: true
   },
-
   images: {
     domains: ['i.pravatar.cc', 'coffins-direct.co.uk', 'bellacouche.com',
       'res.cloudinary.com',
